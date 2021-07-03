e110385c0498e76a5d477b4625309b1d60165612
Mohyaddin Saleh <mo7y..66@gmail.com>
2021-07-03 21:19:15 +0300

    Supported adding action buttons to columns via slots.

--- src/column.vue
+++ src/column.vue
@@ -1,8 +1,13 @@
 <template>
-  <th><slot></slot></th>
+  <th>
+    <slot></slot>
+    <slot name="actions" v-if="renderActionsTemplate"></slot>
+  </th>
 </template>
 
 <script>
+var $ = window.$;
+
 export default {
   name: 'DtColumn',
   props: {
@@ -35,13 +40,36 @@ export default {
       default: false
     }
   },
+  data(){
+    return {
+      // A temporary hack to create the actual DOM of the action buttons if injected to its slot by the user.
+      // Which will be used in the actions column for each fetched datatable row.
+      renderActionsTemplate: true
+    };
+  },
+  methods: {
+    renderActions(row){
+      var actions = [], element, idKey;
+      this.$slots.actions().forEach(function(action){
+        element = $(action.el);
+        idKey = element.attr('data-id');
+        element.attr('data-id', row[(!idKey)? 'id' : idKey]);
+        actions.push($('<div/>').append(element).html());
+      });
+      return actions.join(' ');
+    }
+  },
   mounted(){
     var column = {};
     var props = ['data', 'render', 'name', 'searchable', 'orderable', 'visible', 'defaultContent', 'className', 'width'];
     for(var i in props)
       if(this.$props[props[i]] !== undefined)
         column[props[i]] = this.$props[props[i]];
+    if(this.$slots.actions !== undefined)
+      column.data = this.renderActions;
     this.$parent.columns.push(column);
+    // Removes the action column elements template form DOM after it's rendered.
+    this.renderActionsTemplate = false;
     if(this.last)
       this.$parent.init();
   }
